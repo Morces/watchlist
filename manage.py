5bcc9f146e4e7fcba343ff51389639e07d169586
Moses Karani <karanim594@gmail.com>
2022-05-06 13:32:05 +0300

    database initiation and implementation

--- manage.py
+++ manage.py
@@ -1,10 +1,16 @@
-from app import create_app
-from flask_script import Manager,Server
+
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
+from app import create_app, db
+from app.models import Role, User
 
 # Creating app instance
 app = create_app('development')
-
+migrate = Migrate(app,db)
 manager = Manager(app)
+
+
 manager.add_command('server',Server)
 @manager.command
 def test():
@@ -12,6 +18,12 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+manager.add_command('db', MigrateCommand)
+@manager.shell 
+def make_shell_context():
+    return dict(app=app,db=db,User=User, Role=Role)
 
 if __name__ == '__main__':
     manager.run()
